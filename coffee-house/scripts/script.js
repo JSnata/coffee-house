ea202a76f1b3874c44aea791864a5864d62dda35
Natallia Kulikouskaya <n.kulikouskaya@gmail.com>
2023-12-06 17:25:43 +0100

    feat: add basic popup styles and functionality

--- coffee-house/scripts/script.js
+++ coffee-house/scripts/script.js
@@ -1,3 +1,5 @@
+// Burger menu
+
 const menuToggle = document.querySelector(".nav-toggle-block");
 const buttonToggle = document.querySelector(".nav-toggle-button");
 const navMenu = document.querySelector(".nav-container");
@@ -6,7 +8,7 @@ const links = document.querySelectorAll(".main-nav a");
 buttonToggle.addEventListener("click", () => {
   buttonToggle.classList.toggle("close");
   navMenu.classList.toggle("active");
-  document.body.classList.toggle("menu-open");
+  document.body.classList.toggle("popup-opened");
 });
 
 links.forEach((link) => {
@@ -14,9 +16,32 @@ links.forEach((link) => {
     e.preventDefault();
     navMenu.classList.remove("active");
     buttonToggle.classList.remove("close");
-    document.body.classList.remove("menu-open");
+    document.body.classList.remove("popup-opened");
     setTimeout(() => {
       window.location.href = link.href;
     }, 300);
   });
+});
+
+// Menu modal window
+
+const modalOpenButton = document.getElementById("modalOpenButton");
+const modal = document.getElementById("menuModal");
+const modalCloseButton = document.getElementById("modalCloseButton");
+
+modalOpenButton.addEventListener("click", () => {
+  modal.style.display = "flex";
+  document.body.classList.add("popup-opened");
+});
+
+modalCloseButton.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.classList.remove("popup-opened");
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    document.body.classList.remove("popup-opened");
+  }
 });
