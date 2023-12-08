ea21d920705614a76359e995fb3d02bd270e4c68
Natallia Kulikouskaya <n.kulikouskaya@gmail.com>
2023-12-08 11:59:43 +0100

    refactor: split code into separate functions

--- coffee-house/scripts/script.js
+++ coffee-house/scripts/script.js
@@ -1,4 +1,4 @@
-//fetch data 
+//fetch data
 let data = null;
 
 function fetchData() {
@@ -8,7 +8,7 @@ function fetchData() {
       data = jsonData;
     })
     .catch((error) => {
-      console.error('Fetch error:', error);
+      console.error("Fetch error:", error);
     });
 }
 
@@ -40,17 +40,16 @@ links.forEach((link) => {
 // Menu modal window
 
 const modal = document.getElementById("menuModal");
-const loadButtonWrapper = document.querySelector(".load-button-wrapper");
 
 const modalOpenHandler = () => {
   modal.style.display = "flex";
   document.body.classList.add("popup-opened");
-}
+};
 
 const modalCloseHandler = () => {
   modal.style.display = "none";
   document.body.classList.remove("popup-opened");
-}
+};
 
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
@@ -58,11 +57,9 @@ window.addEventListener("click", (event) => {
   }
 });
 
-const fillModal = async (id) => {
+const renderModal = async (id) => {
   const popupData = await data.filter((item) => item.id === +id);
-  console.log(popupData[0].category)
-  // console.log(typeof data[0].id)
-  // console.log(typeof +id)
+
   modal.innerHTML = `
   <div class="menu-modal-container">
   <div class="img-container">
@@ -115,72 +112,86 @@ const fillModal = async (id) => {
 
 // Menu tabs
 
-const insertTabData = (category) => {
-      const filteredData = data.filter((item) => item.category === category);
-      const productList = document.querySelector(".products-list");
-      productList.innerHTML = "";
+const loadButtonWrapper = document.querySelector(".load-button-wrapper");
+const productList = document.querySelector(".products-list");
 
-      filteredData.forEach((product, index) => {
-        const productCard = document.createElement("li");
-        productCard.classList.add("product-card");
-        productCard.id = `${product.id}`;
+const createProductCard = (product, index) => {
+  const productCard = document.createElement("li");
+  productCard.classList.add("product-card");
+  productCard.id = `${product.id}`;
 
-        productCard.addEventListener('click', (event) => {
+  productCard.addEventListener("click", (event) => {
+    id = event.target.closest(".product-card").id;
+    renderModal(id);
+    modalOpenHandler();
+  });
 
-          id = event.target.closest(".product-card").id;
-          console.log(id)
-          fillModal(id);
-          modalOpenHandler();
-      });
+  if (window.innerWidth <= 768 && index >= 4) {
+    productCard.classList.add("hidden");
+  }
 
-        if (window.innerWidth <= 768 && index >= 4) {
-          productCard.classList.add("hidden");
-        }
-        if (window.innerWidth > 768) {
-          const loadButtonWrapper = document.querySelector(
-            ".load-button-wrapper"
-          );
-        
-          loadButtonWrapper.innerHTML = '';
-        }
-        productCard.innerHTML = `
-          <div class="img-container">
-                  <img src="./assets/img/${product.category}-${
-          index + 1
-        }.jpg" alt="" >
-                </div>
-                <div class="content">
-                  <h3 class="product-title">${product.name}</h3>
-                  <p class="product-description">
-                    ${product.description}
-                  </p>
-                  <p class="product-price">${product.price}</p>
+  productCard.innerHTML = `
+    <div class="img-container">
+            <img src="./assets/img/${product.category}-${
+    index + 1
+  }.jpg" alt="" >
           </div>
-          `;
-        productList.appendChild(productCard);
-      });
+          <div class="content">
+            <h3 class="product-title">${product.name}</h3>
+            <p class="product-description">
+              ${product.description}
+            </p>
+            <p class="product-price">${product.price}</p>
+    </div>
+    `;
 
-      if (window.innerWidth <= 768 && filteredData.length > 4) {
-        const loadButtonWrapper = document.querySelector(
-          ".load-button-wrapper"
-        );
-        loadButtonWrapper.innerHTML = `<button type="button" class="load-button">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g id="refresh">
-            <path id="Ellipse" d="M21.8883 13.5C21.1645 18.3113 17.013 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C16.1006 2 19.6248 4.46819 21.1679 8" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
-            <path id="Ellipse_2" d="M17 8H21.4C21.7314 8 22 7.73137 22 7.4V3" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
-          </g>
-        </svg>  
-      </button>`;
+  return productCard;
+};
 
-        const loadButton = document.querySelector(".load-button");
-        loadButton.addEventListener("click", () => {
-          productList.querySelectorAll("li:nth-child(n+5)").forEach((item) => {
-            item.style.display = "block";
-          });
-          loadButton.style.display = "none";
-        });
-      }
+const loadButtonClickHandler = (e) => {
+  productList.querySelectorAll("li:nth-child(n+5)").forEach((item) => {
+    item.style.display = "block";
+  });
+  e.target.style.display = "none";
+}
+
+const showLoadButton = () => {
+  loadButtonWrapper.innerHTML = `<button type="button" class="load-button" onclick="loadButtonClickHandler(event)">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="refresh">
+      <path id="Ellipse" d="M21.8883 13.5C21.1645 18.3113 17.013 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C16.1006 2 19.6248 4.46819 21.1679 8" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
+      <path id="Ellipse_2" d="M17 8H21.4C21.7314 8 22 7.73137 22 7.4V3" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+  </svg>  
+  </button>`;
+}
+
+const hideLoadButton = () => {
+  loadButtonWrapper.innerHTML = "";
+}
+
+const insertTabData = (category) => {
+  // filter data
+  const filteredData = data.filter((item) => item.category === category);
+  // clean up products list before new render
+  productList.innerHTML = "";
+  // render products for category
+  filteredData.forEach((product, index) => {
+    const productCard = createProductCard(product, index);
+    productList.appendChild(productCard);
+  });
+
+  console.log(window.innerWidth)
+  if (window.innerWidth > 768) {
+    hideLoadButton();
+  }
+
+  if (window.innerWidth <= 768 && filteredData.length > 4) {
+    console.log('hey');
+    console.log(filteredData);
+    console.log(filteredData.length)
+    showLoadButton();
+  }
 };
 
 function handleTabClick(event) {
@@ -188,20 +199,13 @@ function handleTabClick(event) {
 
   const tabButtons = document.querySelectorAll(".menu-tabs .tab-item");
   tabButtons.forEach((button) => {
-    
     button.classList.remove("active");
   });
 
   const container = event.target.closest(".tab-item");
   container.classList.add("active");
-
-  insertTabData(category, window.innerWidth <= 768);
-
-  const loadButtonWrapper = document.querySelector(
-    ".load-button-wrapper"
-  );
-
-  loadButtonWrapper.innerHTML = '';
+  hideLoadButton();
+  insertTabData(category);
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -209,7 +213,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   insertTabData("coffee");
 });
 
-window.addEventListener('resize', () => {
-  const activeButton = document.querySelector('.menu-tabs .tab-item.active button');
+window.addEventListener("resize", () => {
+  const activeButton = document.querySelector(
+    ".menu-tabs .tab-item.active button"
+  );
   insertTabData(activeButton.dataset.category);
 });
