c1e40d1a4e5b2a690ef9bc09afac41092d3cf49d
Natallia Kulikouskaya <n.kulikouskaya@gmail.com>
2023-12-11 12:57:46 +0100

    feat: add slider functionality

--- coffee-house/scripts/script.js
+++ coffee-house/scripts/script.js
@@ -38,6 +38,7 @@ links.forEach((link) => {
 });
 
 // Menu modal window
+
 let popupPrice = 0;
 let sizeCounter = 0;
 let additivesCounter = 0;
@@ -53,7 +54,8 @@ const modalCloseHandler = () => {
   modal.style.display = "none";
   document.body.classList.remove("popup-opened");
   popupPrice = 0;
-  changedPrice = 0;
+  sizeCounter = 0;
+  additivesCounter = 0;
 };
 
 window.addEventListener("click", (event) => {
@@ -145,8 +147,7 @@ const handleProductAdditivesClick = (e) => {
   const currentValue = Number(e.target.dataset.value);
 
   if (e.target.closest(".tab-item").classList.contains("active")){
-    additivesCounter += currentValue;
-    
+    additivesCounter += currentValue; 
   } else {
     additivesCounter -= currentValue;
   };
@@ -259,5 +260,169 @@ window.addEventListener("resize", () => {
   );
   insertTabData(activeButton.dataset.category);
 });
+
+// Slider
+
+  const slider = document.querySelector('.slider-block');
+  const sliderList = slider.querySelector('.list');
+  const slides = slider.querySelectorAll('.slide');
+  const buttonNextNode = slider.querySelector('.arrow.--next');
+  const buttonPrevNode = slider.querySelector('.arrow.--prev');
+  const progressBar = slider.querySelector('.progress-bar');
+  const bullets = progressBar.querySelectorAll('.progress-control');
+
+
+  let currentSlide = 0;
+
+  let slidesLength = slides.length - 1;
+
+// Bullets
+
+  const setBullet = () => {
+      bullets.forEach((bullet, index) => {
+          if (currentSlide === index) {
+              bullet.classList.add('current');
+          } else {
+              bullet.classList.remove('current');
+              bullet.firstElementChild.style.width = '0%';
+          }
+      });
+  }
+
+  const prepareSlider = () => {
+  
+      slides.forEach((slide, index) => {
+          slide.style.transform = `translateX(${index * 100}%)`;
+      });
+
+      setBullet();
+  }
+
+  const toNextSlide = () => {
+      if (currentSlide === slidesLength) {
+          currentSlide = 0;
+      } else {
+          currentSlide++;
+      }
+
+      slides.forEach((slide, index) => {
+          slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+      });
+
+      progressTime = 0;
+      setBullet();
+  }
+
+  const toPrevSlide = () => {
+      if (currentSlide === 0) {
+          currentSlide = slidesLength;
+      } else {
+          currentSlide--;
+      }
+
+      slides.forEach((slide, index) => {
+          slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+      });
+
+      progressTime = 0;
+      setBullet();
+  }
+
+  buttonNextNode.addEventListener('click', () => {
+      toNextSlide();
+  });
+  buttonPrevNode.addEventListener('click', () => {
+      toPrevSlide();
+  });
+
+  
+  //Swipe
+  const swipeDistance = 100; // Min swipe distance
+  let startSwipe = null;
+
+  const calculateSwipeDirection = (direction) => {
+      let difference = startSwipe - direction;
+
+      if (Math.abs(difference) > swipeDistance) {
+          difference > 0 ? toNextSlide() : toPrevSlide();
+
+          startSwipe = null;
+      }
+  }
+
+  sliderList.addEventListener('touchstart', (e) => {
+      startSwipe = e.touches[0].clientX;
+  });
+  sliderList.addEventListener('mousedown', (e) => {
+      startSwipe = e.clientX;
+  });
+
+  sliderList.addEventListener('touchmove', (e) => {
+      if (!startSwipe) {
+          return;
+      }
+
+      calculateSwipeDirection(e.touches[0].clientX);
+  });
+
+  sliderList.addEventListener('mousemove', (e) => {
+      if (!startSwipe) {
+          return;
+      }
+
+      calculateSwipeDirection(e.clientX);
+  });
+
+  sliderList.addEventListener('mouseup', () => {
+      startSwipe = null;
+  });
+
+  // image slider autoplay
+  const AUTOPLAY_SPEED = 5000;
+  let autoplayInterval;
+  let progressPaused = false;
+  let progressTime = 0;
+
+  const runAutoPlay = () => {
+      autoplayInterval = setInterval(() => {
+          if(!progressPaused) {
+              progressTime++;
+
+              bullets[currentSlide].firstElementChild.style.width = `${Math.round(progressTime * 2)}%`;
+
+              if (progressTime === AUTOPLAY_SPEED / 100) {
+                  bullets[currentSlide].firstElementChild.style.width = `0%`;
+                  toNextSlide();
+                  progressTime = 0;
+              }
+          }
+
+      }, 100);
+  }
+
+
+  //stop the image slider autoplay 
+  sliderList.addEventListener('mouseover', () => {
+      progressPaused = true;
+  });
+
+  sliderList.addEventListener('touchstart', () => {
+      progressPaused = true;
+  });
+
+  //start the image slider autoplay again
+  sliderList.addEventListener('mouseout', () => {
+      progressPaused = false;
+  });
+
+  sliderList.addEventListener('touchend', () => {
+      progressPaused = false;
+  });
+
+
+  // Init slider
+  prepareSlider();
+  runAutoPlay();
+
 
 
